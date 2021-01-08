e60cc07af52a2371d6f80ba8dc89ecfaf80b0b24
mari.kamar <mari.kamar@gmail.com>
2021-01-08 17:11:19 +0300

    adding put test

--- src/test/java/TestRestExample.java
+++ src/test/java/TestRestExample.java
@@ -1,13 +1,16 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+import static net.serenitybdd.rest.RestRequests.*;
 import static org.hamcrest.CoreMatchers.containsString;
 
 
@@ -49,5 +52,41 @@ public class TestRestExample {
 
         logger.info("end test");
 
+    }
+
+    @Test
+    public void firstPutTest() {
+        Random random = new Random();
+        int petId = 1 + random.nextInt(1000 - 1);
+
+        Map<String, String> pet = new HashMap<>();
+        pet.put("id", "" + petId + "");
+        pet.put("name", "test pet");
+
+        logger.info("start test");
+
+       RequestSpecification requestSpecification = new RequestSpecBuilder()
+               .setBaseUri("https://petstore.swagger.io/v2")
+               .addHeader("Content-Type","application/json")
+               .addHeader("Accept", "application/json")
+               .build();
+
+       Response response = given().spec(requestSpecification).body(pet)
+                .when().post("/pet");
+
+       response.prettyPrint();
+
+       int newPetId = response.then().extract().path("id");
+
+        Map<String, String> editPet = new HashMap<>();
+        editPet.put("id", Integer.toString(newPetId));
+        editPet.put("name", "doggie");
+
+        Response editResponse = given().spec(requestSpecification).body(editPet)
+                .when().put("/pet");
+
+        editResponse.prettyPrint();
+
+        logger.info("end test");
     }
 }
